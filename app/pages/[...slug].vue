b6e7f45e383ee89b384fe3b63bc69e2807418ef4
hengxuan-finfare <hengxuand@finfare.com>
2025-12-18 09:11:15 -0800

    adjust design of source of each news item and catch all unknown route to home page

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// Catch-all route for unknown URLs - redirect to home page
+definePageMeta({
+    middleware: () => {
+        return navigateTo('/', { replace: true })
+    }
+})
+</script>
+
+<template>
+    <div></div>
+</template>
